9f4c1f4f5dbd84109cff90a2bcd91e639a39f41f
Ex-Zy <serega1905@ukr.net>
2017-01-12 13:54:14 +0200

    start, add section1

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+// example of simple includes for js
+//=include lib/jquery.min.js
+//=include lib/svgxuse.min.js
+$(function() {
+	//=include partial/menu.js
+	//=include partial/fixedTopper.js
+
+})
---
+++ src/js/partial/menu.js
@@ -0,0 +1,9 @@
+$('.js-menu').click(function() {
+	var hasActive = $(this).hasClass('is-active');
+
+	if(!hasActive) {
+		$(this).addClass('is-active');
+	} else {
+		$(this).removeClass('is-active');
+	}
+});
