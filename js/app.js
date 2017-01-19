74b8c2725f5de56e8d2385ca3903bc86d93009bc
DESKTOP-DH8PFBKsereg <serega1905@ukr.net>
2017-01-19 22:51:09 +0200

    upd

--- js/app.js
+++ js/app.js
@@ -2359,4 +2359,23 @@ $(function () {
 		$("body").removeClass("is-hidden");
 		return false;
 	});
+
+	function showAnimation() {
+		var scroll = $(window).scrollTop();
+		var section = $('.js-animate');
+		var win = $(window).scrollTop() + $(window).outerHeight() - 200;
+
+		section.each(function () {
+			var top = $(this).offset().top;
+			if (win >= top) {
+				$(this).addClass('is-animate');
+			}
+		});
+	}
+
+	showAnimation();
+
+	$(window).scroll(function () {
+		showAnimation();
+	});
 });
