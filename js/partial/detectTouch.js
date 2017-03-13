a95efdf5bc6293b4531640235f99cbde9481fbe8
DESKTOP-DH8PFBKsereg <serega1905@ukr.net>
2017-03-13 17:26:09 +0200

    upd popup step 2

---
+++ js/partial/detectTouch.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var touch = 'ontouchstart' in window;
+
+!touch ? $('body').addClass('is-no-touch') : $('body').addClass('is-touch');
---
+++ js/partial/touch.js
@@ -0,0 +1,3 @@
+'use strict';
+
+var touch = 'ontouchstart' in window;
