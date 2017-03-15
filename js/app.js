f5d640eb95365b6d58c8fb485a09fbd8b009826b
DESKTOP-DH8PFBKsereg <serega1905@ukr.net>
2017-03-15 11:00:13 +0200

    upd

--- js/app.js
+++ js/app.js
@@ -2518,7 +2518,7 @@ $(function () {
 	$('.navigation a').on('click', function (e) {
 		e.preventDefault();
 
-		$('.navigation a').removeClass('is-active');
+		$('.navigation a').add('.js-sidebar').removeClass('is-active');
 		$(this).addClass('is-active');
 
 		var section = $(this).attr('href');
