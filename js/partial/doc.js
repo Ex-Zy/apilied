5e2e4ae70a61b95c17bdf69ac03831ff1b4556be
DESKTOP-DH8PFBKsereg <serega1905@ukr.net>
2017-05-30 00:56:58 +0300

    design update

--- js/partial/doc.js
+++
@@ -1,8 +0,0 @@
-$(document).click(function(e) {
-	var notSidebarAndNotMenu = !$(e.target).closest('.js-sidebar').length &&
-							   !$(e.target).closest('.js-menu').length;
-
-	if(notSidebarAndNotMenu) {
-		$('.js-sidebar').removeClass('is-active');
-	}
-});
