a55ba9fac46ae09c0608b074874961e24327487a
DESKTOP-DH8PFBK\sereg <serega1905@ukr.net>
2017-05-30 10:40:12 +0300

    production

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,7 @@ import svg4everybody from 'svg4everybody';
 svg4everybody();
 
 // custom scrollbar
-import perfectScrollbar from './lib/perfect-scrollbar.jquery.min.js';
+import './lib/perfect-scrollbar.jquery.min.js';
 
 //common scripts
 require('./_index.js');
