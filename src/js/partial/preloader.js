e6481793f0029c944b5a26390ccc3f1ecf384654
DESKTOP-DH8PFBK\sereg <serega1905@ukr.net>
2017-05-31 17:05:43 +0300

    fade title for preloader

--- src/js/partial/preloader.js
+++ src/js/partial/preloader.js
@@ -4,8 +4,5 @@ let time = 500;
 let speed = 600;
 
 WIN.on('load', () => {
-	PRELOADER.delay(time)
-			 .fadeOut(speed)
-			 .find('.js-preloader-title')
-			 .fadeIn(speed);
+	PRELOADER.delay(time).fadeOut(speed);
 });
