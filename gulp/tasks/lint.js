d790a0a185d3bd9a986ac97c2799b3f0bbb14d52
Sergey Nagornykh <serega1905@ukr.net>
2024-07-24 23:42:42 +0300

    fix gulp build

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -1,7 +1,7 @@
 var gulp        = require('gulp');
 var config      = require('../config');
 var htmlhint    = require('gulp-htmlhint');
-var gitStaged   = require("gulp-git-staged");
+// var gitStaged   = require("gulp-git-staged");
 var sassLint    = require('gulp-sass-lint')
 var prettier    = require('gulp-prettier');
 var git         = require('gulp-git');
@@ -16,7 +16,7 @@ gulp.task('lint:js', function() {
 
 gulp.task('prettier', function() {
   return gulp.src(['src/js/**/*.js'])
-    .pipe(gitStaged())
+    // .pipe(gitStaged())
     .pipe(prettier())
     .pipe(gulp.dest('src/js/'))
     .pipe(git.add({args: '-u'}))
@@ -24,7 +24,7 @@ gulp.task('prettier', function() {
 
 gulp.task('lint:html', function() {
   return gulp.src(config.dest.html + "/*.html")
-  	.pipe(gitStaged())
+  	// .pipe(gitStaged())
     .pipe(htmlhint('.htmlhintrc'))
     .pipe(htmlhint.failReporter());
 });
@@ -37,7 +37,7 @@ gulp.task('lint:sass', function() {
 });
 
 gulp.task('lint', [
-    
+
     'lint:sass',
     'lint:html'
 ]);
